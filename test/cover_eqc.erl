7119dadd50562a24ee27ef1947dd25e75fa9f504
Thomas Arts <thomas.arts@quviq.com>
2014-11-02 19:22:55 +0100

    Compile cover.erl as part of the test

--- test/cover_eqc.erl
+++ test/cover_eqc.erl
@@ -7,6 +7,22 @@
 
 -define(TEST_MODULE, myerlangprog).
 
+%% Cover compile cover with eqc_cover
+%% Tricky, since cover.erl has its own parse transform
+
+compile() ->
+  {Path,_} = filename:find_src(cover),
+  Options = os:getenv("ERL_COMPILER_OPTIONS"),
+  os:putenv("ERL_COMPILER_OPTIONS","[]"),
+  compile:file(Path, ['P']),  %% compile with parse_transform
+  ok = file:rename("cover.P","cover.erl"),
+  case Options of 
+    false -> os:putenv("ERL_COMPILER_OPTIONS","[]");
+    _ -> os:putenv("ERL_COMPILER_OPTIONS",Options)
+  end,
+  compile:file("cover.erl").
+
+
 prop_cover() ->
   ?FORALL(Code, eqc_erlang_program:module(?TEST_MODULE,[{maps,true},{macros,true}]),
 	  begin
